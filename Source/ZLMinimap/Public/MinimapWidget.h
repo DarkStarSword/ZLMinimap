5d8459a6555a85b3c9dbce8224e73c1740926d9c
Ian Munsie <darkstarsword@gmail.com>
2019-03-16 16:11:10 +1100

    Add minimap widget C++ class
    This reparents the minimap widget blueprint on a C++ class with a
    blueprint implementable function to return the panel area to C++ code
    that will be used to display the icons on the minimap.
    
    This allows the minimap UI to be customised to suit the game while still
    allowing the C++ code to draw the icons in the right positions. To
    demonstrate this, a simple black border is added around the minimap.

---
+++ Source/ZLMinimap/Public/MinimapWidget.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "MinimapWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Blueprintable)
+class ZLMINIMAP_API UMinimapWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+public:
+
+	// Assign the panel area that will be used to display minimap icons via this:
+	UFUNCTION(BlueprintImplementableEvent, Category="Minimap")
+	UPanelWidget *GetMinimapIconArea() const;
+};
