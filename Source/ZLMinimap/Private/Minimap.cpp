65130ba3beb63a7169da9ff305d1bce308b78d59
Ian Munsie <darkstarsword@gmail.com>
2019-03-17 13:37:08 +1100

    Position minimap icons on each actor
    This iterates through every actor and adds a blank icon for each one to
    the minimap, matching the translation and rotation applied to the map's
    background. This is currently done every frame to prove things out, but
    will be optimised in a later commit.

--- Source/ZLMinimap/Private/Minimap.cpp
+++ Source/ZLMinimap/Private/Minimap.cpp
@@ -30,14 +30,60 @@ void AMinimap::BeginPlay()
 		UE_LOG(LogMinimap, Warning, TEXT("Minimap widget's icon area panel is not set"));
 		return;
 	}
-
-	UImage *test_icon = NewObject<UImage>();
-	minimap_panel->AddChild(test_icon);
 }
 
 // Called every frame
 void AMinimap::Tick(float DeltaTime)
 {
+	APlayerController *player;
+	FVector2D panel_pivot;
+	FVector2D panel_size;
+	FVector2D player_loc;
+	FRotator camera_rot;
+	FVector camera_pos;
+	float panel_scale;
+	UWorld *world;
+	APawn *pawn;
+
 	Super::Tick(DeltaTime);
+
+	if (!minimap_panel)
+		return;
+
+	// For now I'm adding new icons and iterating all actors each frame.
+	// Once the transformation works I'll optimise this
+	minimap_panel->ClearChildren();
+	// TODO: world->AddOnActorSpawnedHandler
+
+	world = GetWorld();
+	if (!world)
+		return;
+
+	// TODO: Allow the minimap to be centered on an arbitrary actor
+	player = world->GetFirstPlayerController();
+	if (!player)
+		return;
+
+	pawn = player->GetPawn();
+	if (!pawn)
+		return;
+
+	// FIXME: Tie vertical offset and render size with screen capture component:
+	panel_size = minimap_panel->GetCachedGeometry().GetLocalSize();
+	panel_pivot = panel_size * FVector2D(0.5, 0.75);
+	panel_scale = panel_size.X / 2048.0f;
+
+	player->GetPlayerViewPoint(camera_pos, camera_rot);
+	player_loc = FVector2D(pawn->GetActorLocation());
+
+	for (TActorIterator<AActor> it(world); it; ++it) {
+		UImage *icon = NewObject<UImage>();
+		UCanvasPanelSlot *slot = Cast<UCanvasPanelSlot>(minimap_panel->AddChild(icon));
+		FVector2D icon_loc = FVector2D(it->GetActorLocation()) - player_loc;
+		// TODO: Check if possibly in range before applying rotation
+		slot->SetPosition(icon_loc.GetRotated(270 - camera_rot.Yaw) * panel_scale + panel_pivot);
+		slot->SetAlignment(FVector2D(0.5, 0.5));
+		slot->SetSize(FVector2D(20, 20));
+	}
 }
 
